3cc9167cdf6445a37e54942689af57a6f3395af4
Lathrell Pagsuguiron <lathrellxd@gmail.com>
2022-09-13 22:48:23 +0800

    form

--- main.js
+++ main.js
@@ -1,11 +1,74 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "300px";
   document.getElementById("main").style.marginLeft = "300px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  document.getElementById("parent").style.marginLeft = "300px";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
-  document.body.style.backgroundColor = "white";
+  document.getElementById("parent").style.marginLeft = "0";
+}
+
+/* Neumorphism Login Form */
+
+let switchCtn = document.querySelector("#switch-cnt");
+let switchC1 = document.querySelector("#switch-c1");
+let switchC2 = document.querySelector("#switch-c2");
+let switchCircle = document.querySelectorAll(".switch__circle");
+let switchBtn = document.querySelectorAll(".switch-btn");
+let aContainer = document.querySelector("#a-container");
+let bContainer = document.querySelector("#b-container");
+let allButtons = document.querySelectorAll(".submit");
+
+let getButtons = (e) => e.preventDefault();
+
+let changeForm = (e) => {
+  switchCtn.classList.add("is-gx");
+  setTimeout(function () {
+    switchCtn.classList.remove("is-gx");
+  }, 1500);
+
+  switchCtn.classList.toggle("is-txr");
+  switchCircle[0].classList.toggle("is-txr");
+  switchCircle[1].classList.toggle("is-txr");
+
+  switchC1.classList.toggle("is-hidden");
+  switchC2.classList.toggle("is-hidden");
+  aContainer.classList.toggle("is-txl");
+  bContainer.classList.toggle("is-txl");
+  bContainer.classList.toggle("is-z200");
+};
+
+let mainF = (e) => {
+  for (var i = 0; i < allButtons.length; i++)
+    allButtons[i].addEventListener("click", getButtons);
+  for (var i = 0; i < switchBtn.length; i++)
+    switchBtn[i].addEventListener("click", changeForm);
+};
+
+window.addEventListener("load", mainF);
+
+/* Smooth Scroll */
+function smoothScroll(target, duration) {
+  var target = document.querySelector(".bg");
+  var targetPosition = target.getBoundingClientRect().top;
+  var startPosition = window.pageYOffset || window.scrollY;
+  var distance = targetPosition - startPosition;
+  var startTime = null;
+
+  function loop(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    var timeElapsed = currentTime - startTime;
+    var run = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) requestAnimationFrame(loop);
+  }
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  }
+  requestAnimationFrame(loop);
 }
